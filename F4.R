254233ff1de350ad0b98a9c6b04b0adcd0e8143e
zzww <zhaowei21@mails.ucas.ac.cn>
2025-06-16 17:24:17 +0800

    Update F4.R

--- F4.R
+++ F4.R
@@ -6,20 +6,29 @@ setwd("C:\\Users\\User\\Desktop\\")
 #########Figure 4a
 Rt<-read.csv("Annual Dataset.csv",encoding="UTF-8") 
 Rt<-subset(Rt,Rt$MAT<3.75 & Rt$SOC!=39.4 & Rt$flux!=1812)
-model<-psem(
-lm(flux ~LAI+SOC+SWC,  na.action = na.omit,data = Rt),
-lm(SOC ~ LAI+MAT,  na.action = na.omit,data = Rt),
-lm(LAI ~SWC+MAT, na.action = na.omit,data = Rt))
-summary(model, .progressBar = F)
-plot(model)
+nrow(Rt)
+colnames(Rt)
+mode_new <- psem(
+  lm(flux ~ LAI+SWC , data = Rt, na.action = na.omit),
+  lm(SOC ~ MAT, data = Rt, na.action = na.omit),
+  lm(LAI ~ MAT+SWC, data = Rt, na.action = na.omit),
+lm(SWC ~ MAT, data = Rt, na.action = na.omit),
+  flux %~~% SOC)
+summary(mode_new, .progressBar = F)
+plot(mode_new)
 
 
 ######Figure 4c
 Rt<-read.csv("Annual Dataset.csv",encoding="UTF-8")
 Rt<-subset(Rt,Rt$MAT>3.75 )
-model<-psem(
-lm(flux~LAI+MAT+SOC+SWC,  na.action = na.omit,data = Rt),
-lm(SOC ~ MAT+LAI,  na.action = na.omit,data = Rt),
-lm(LAI ~MAT+SWC, na.action = na.omit,data = Rt))
-summary(model, .progressBar = F)
-plot(model)
+nrow(Rt)
+Rt_clean <- na.omit(Rt[, c("flux", "LAI", "SWC", "MAT", "SOC")])
+mode_new <- psem(
+  lm(flux ~ LAI + SWC + MAT, data = Rt_clean),
+  lm(SOC ~ LAI, data = Rt_clean),
+  lm(LAI ~ MAT + SWC, data = Rt_clean),
+  lm(SWC ~ MAT, data = Rt_clean),
+  flux %~~% SOC)
+summary(mode_new, .progressBar = FALSE)
+plot(mode_new)
+
